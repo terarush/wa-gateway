f01365cea195afb4b6548a3b04c85c8b3d84f315
rafia9005 <rafia9005@gmail.com>
2024-12-08 09:48:22 +0700

    init(ejs): generate views with ejs view engine

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
-const router = require('./routes/routes'); 
+const router = require('./routes/routes');
 const whatsappClient = require('./client/whatsapp-client');
+const qrcode = require("qrcode-terminal")
 
 dotenv.config();
 
@@ -11,7 +12,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
 app.use(router);
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'WhatsApp Gateway', message: 'Welcome to WhatsApp Gateway!' });
+});
 
 const PORT = process.env.PORT || 3000;
 whatsappClient.on('qr', (qr) => qrcode.generate(qr, { small: true }));
